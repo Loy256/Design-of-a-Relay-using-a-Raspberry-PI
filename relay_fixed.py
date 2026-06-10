5c6710aeca1c6a29c20c0b902306b6a70ec929e5
Loy256 <125868126+Loy256@users.noreply.github.com>
2026-06-10 14:33:52 +0300

    Update relay_fixed.py

--- relay_fixed.py
+++ relay_fixed.py
@@ -164,7 +164,7 @@ class VirtualDisc:
     """Virtual disc for IDMT timing"""
     def __init__(self):
         self.position = 0.0
-        self.HALF_CYCLE_DURATION = 0.2 / (2 * SYSTEM_FREQUENCY)
+        self.HALF_CYCLE_DURATION = 1.0 / (2 * SYSTEM_FREQUENCY)
     
     def reset(self):
         self.position = 0.0
